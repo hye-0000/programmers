45ecf8da28661579960338a0d52869458de2da1e
hye-0000 <hye_0000@naver.com>
2023-04-11 17:21:58 +0900

    feat(0411): 0411 문제

---
+++ src/main/java/com/hy/level0/p120912/Main.java
@@ -0,0 +1,22 @@
+package com.hy.level0.p120912;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        String str = Arrays.toString(array);
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) == '7'){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
