fdee4cdcd4a700f7b7bf8d59fc8cb99923a19b52
hye-0000 <hye_0000@naver.com>
2023-04-17 17:11:13 +0900

    실패하는 테스트 작성

---
+++ src/main/java/com/hy/level2/p87377/Main.java
@@ -0,0 +1,15 @@
+package com.hy.level2.p87377;
+
+public class Main {
+}
+
+class Solution {
+    public String[] solution(int[][] line) {
+        String[] answer = {};
+        return answer;
+    }
+
+    public int[] intersection(int[] line1, int[] line2) {
+        return null;
+    }
+}
---
+++ src/test/java/com/hy/level2/p87377/Tests.java
@@ -0,0 +1,15 @@
+package com.hy.level2.p87377;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class Tests {
+    @Test
+    @DisplayName("교점, [[1, -1, 0], [2, -1, 0]]")
+    void t1(){
+        assertThat(new Solution().intersection(new int[]{1, -1, 0}, new int[]{2, -1, 0})).isEqualTo(new int[] {0, 0});
+    }
+
+}
