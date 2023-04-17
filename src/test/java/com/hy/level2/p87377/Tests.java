848634c868ed848f4a70816b089a0e387379506d
hye-0000 <hye_0000@naver.com>
2023-04-17 17:12:48 +0900

    교점 구현

--- src/test/java/com/hy/level2/p87377/Tests.java
+++ src/test/java/com/hy/level2/p87377/Tests.java
@@ -8,8 +8,41 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class Tests {
     @Test
     @DisplayName("교점, [[1, -1, 0], [2, -1, 0]]")
-    void t1(){
-        assertThat(new Solution().intersection(new int[]{1, -1, 0}, new int[]{2, -1, 0})).isEqualTo(new int[] {0, 0});
+    void t1() {
+        assertThat(
+                new Solution().intersection(
+                        new int[]{1, -1, 0},
+                        new int[]{2, -1, 0}
+                )
+        ).isEqualTo(
+                new long[]{0, 0}
+        );
+    }
+
+    @Test
+    @DisplayName("교점, [[1, -1, 0], [4, -1, 0]]")
+    void t1_2() {
+        assertThat(
+                new Solution().intersection(
+                        new int[]{1, -1, 0},
+                        new int[]{4, -1, 0}
+                )
+        ).isEqualTo(
+                new long[]{0, 0}
+        );
+    }
+
+    @Test
+    @DisplayName("교점, [[2, -1, 0], [4, -1, 0]]")
+    void t1_3() {
+        assertThat(
+                new Solution().intersection(
+                        new int[]{2, -1, 0},
+                        new int[]{4, -1, 0}
+                )
+        ).isEqualTo(
+                new long[]{0, 0}
+        );
     }
 
 }
