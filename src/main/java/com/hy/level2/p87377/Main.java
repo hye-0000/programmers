6f066c1f26d6f4528965e30789d9106717538ecc
hye-0000 <hye_0000@naver.com>
2023-04-18 13:58:54 +0900

    default 접근 제어자 사용

--- src/main/java/com/hy/level2/p87377/Main.java
+++ src/main/java/com/hy/level2/p87377/Main.java
@@ -18,7 +18,7 @@ class Solution {
         return drawOnCoordinate(matrix);
     }
 
-    public Point intersection(int[] line1, int[] line2) {
+    Point intersection(int[] line1, int[] line2) {
         // Ax + By + E = 0
         double A = line1[0];
         double B = line1[1];
@@ -45,7 +45,7 @@ class Solution {
         return Point.of(x, y);
     }
 
-    public Points intersections(int[][] line) {
+    Points intersections(int[][] line) {
        Points points = Points.of();
 
         for (int i = 0; i < line.length; i++) {
@@ -64,7 +64,7 @@ class Solution {
 
 
 
-    public String[] drawOnCoordinate(char[][] matrix) {
+    String[] drawOnCoordinate(char[][] matrix) {
         return Ut.revRange(0, matrix.length)
                 .boxed()
                 .map(i -> matrix[i])
@@ -170,7 +170,7 @@ class Points implements Iterable<Point>{
         return data.stream();
     }
 
-    public Point getMinPoint() {
+    Point getMinPoint() {
         long x = Long.MAX_VALUE;
         long y = Long.MAX_VALUE;
 
@@ -182,7 +182,7 @@ class Points implements Iterable<Point>{
         return Point.of(x, y);
     }
 
-    public Point getMaxPoint() {
+    Point getMaxPoint() {
         long x = Long.MIN_VALUE;
         long y = Long.MIN_VALUE;
 
@@ -194,7 +194,7 @@ class Points implements Iterable<Point>{
         return Point.of(x, y);
     }
 
-    public Points positivePoints() {
+    Points positivePoints() {
         Point minPoint = getMinPoint();
 
         return Points.of(
@@ -204,7 +204,7 @@ class Points implements Iterable<Point>{
         );
     }
 
-    public char[][] emptyMatrix() {
+    char[][] emptyMatrix() {
         Point minPoint = getMinPoint();
         Point maxPoint = getMaxPoint();
 
@@ -229,7 +229,7 @@ class Points implements Iterable<Point>{
 }
 
 class Ut {
-    static IntStream revRange(int from, int to) {
+    public static IntStream revRange(int from, int to) {
         return IntStream.range(from, to)
                 .map(i -> to - i + from - 1);
     }
