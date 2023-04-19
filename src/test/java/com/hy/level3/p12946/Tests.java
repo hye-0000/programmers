b13c848b5d093fa3eba6c3f0c71a4eefc2504e01
hye-0000 <hye_0000@naver.com>
2023-04-19 13:20:32 +0900

    N=2 우

--- src/test/java/com/hy/level3/p12946/Tests.java
+++ src/test/java/com/hy/level3/p12946/Tests.java
@@ -9,23 +9,43 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @TestMethodOrder(MethodOrderer.MethodName.class)
 public class Tests {
-        @Test
-        @DisplayName("n=1 => [[1,3]]")
-        void t01(){
+    @Test
+    @DisplayName("n=1 => [[1,3]]")
+    void t01(){
         assertThat(
                 new Solution().solution(1)
         ).isEqualTo(
                 new int[][]{{1, 3}}
         );
-        }
+    }
 
-        @Test
-        @DisplayName("n=2 => [[1,2], [1,3], [2,3]]")
-        void t02(){
+    @Test
+    @DisplayName("n=2 => [[1,2], [1,3], [2,3]]")
+    void t02(){
         assertThat(
                 new Solution().solution(2)
         ).isEqualTo(
                 new int[][]{{1, 2}, {1, 3}, {2, 3}}
         );
-        }
+    }
+
+    @Test
+    @DisplayName("from=1, to=2, n=2 => [[1,3], [1,2], [3,2]]")
+    void t03() {
+        assertThat(
+                new Hanoi(1, 2, 2).toArray()
+        ).isEqualTo(
+                new int[][]{{1, 3}, {1, 2}, {3, 2}}
+        );
+    }
+
+    @Test
+    @DisplayName("from=2, to=1, n=2 => [[2,3], [2,1], [3,1]]")
+    void t04() {
+        assertThat(
+                new Hanoi(2, 1, 2).toArray()
+        ).isEqualTo(
+                new int[][]{{2, 3}, {2, 1}, {3, 1}}
+        );
+    }
 }
