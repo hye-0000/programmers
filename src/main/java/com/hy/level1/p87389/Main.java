6dfc38ebaecadbf9eb88566781ea206cc4449667
hye-0000 <hye_0000@naver.com>
2023-04-21 16:34:08 +0900

    0421

---
+++ src/main/java/com/hy/level1/p87389/Main.java
@@ -0,0 +1,16 @@
+package com.hy.level1.p87389;
+
+public class Main {
+}
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i = 2; i < n; i++){
+            if(n % i == 1){
+                answer = i;
+                break;
+            }
+        }
+        return answer;
+    }
+}
---
+++ src/main/java/com/hy/level2/p42860/Main.java
@@ -0,0 +1,4 @@
+package com.hy.level2.p42860;
+
+public class Main {
+}
