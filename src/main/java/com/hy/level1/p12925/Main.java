fe81024d328e0cecf70d2ab44d8400bfa1533e64
hye-0000 <hye_0000@naver.com>
2023-04-27 17:03:58 +0900

    0427

---
+++ src/main/java/com/hy/level1/p12925/Main.java
@@ -0,0 +1,11 @@
+package com.hy.level1.p12925;
+
+public class Main {
+}
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        answer = Integer.parseInt(s);
+        return answer;
+    }
+}
